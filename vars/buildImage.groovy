37cf293f810cb696fce2361862f08362d5ad80d3
sailokesh507 <59596991+sailokesh507@users.noreply.github.com>
2025-09-22 13:52:01 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,10 +3,8 @@ def call(String imageName)
 {
     echo "bilding the image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh """
-            docker build -t $imageName .
-            echo \$PASS | docker login -u \$USER --password-stdin
-            docker push $imageName
-        """
+            sh "docker build -t $imageName ."
+            sh 'echo \$PASS | docker login -u \$USER --password-stdin'
+            sh "docker push $imageName"
     }
 }
